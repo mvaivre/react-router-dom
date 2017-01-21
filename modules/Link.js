719a2b1afeff38e03896b5224f898e36dceacdb4
Michael Jackson <mjijackson@gmail.com>
2017-01-21 11:24:34 -0800

    Add react-router-dom package

---
+++ modules/Link.js
@@ -0,0 +1,70 @@
+import React, { PropTypes } from 'react'
+
+const isLeftClickEvent = (event) =>
+  event.button === 0
+
+const isModifiedEvent = (event) =>
+  !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+
+/**
+ * The public API for a history-aware <a>.
+ */
+class Link extends React.Component {
+  static contextTypes = {
+    router: PropTypes.shape({
+      history: PropTypes.shape({
+        createHref: PropTypes.func.isRequired,
+        push: PropTypes.func.isRequired,
+        replace: PropTypes.func.isRequired
+      }).isRequired
+    }).isRequired
+  }
+
+  static propTypes = {
+    onClick: PropTypes.func,
+    target: PropTypes.string,
+    replace: PropTypes.bool,
+    to: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ])
+  }
+
+  static defaultProps = {
+    replace: false
+  }
+
+  handleClick = (event) => {
+    if (this.props.onClick)
+      this.props.onClick(event)
+
+    if (
+      !event.defaultPrevented && // onClick prevented default
+      !this.props.target && // let browser handle "target=_blank" etc.
+      !isModifiedEvent(event) &&
+      isLeftClickEvent(event)
+    ) {
+      event.preventDefault()
+
+      const { history } = this.context.router
+      const { replace, to } = this.props
+
+      if (replace) {
+        history.replace(to)
+      } else {
+        history.push(to)
+      }
+    }
+  }
+
+  render() {
+    const { replace, to, ...props } = this.props // eslint-disable-line no-unused-vars
+    const href = this.context.router.history.createHref(
+      typeof to === 'string' ? { pathname: to } : to
+    )
+
+    return <a {...props} onClick={this.handleClick} href={href}/>
+  }
+}
+
+export default Link
---
+++ modules/NavLink.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react'
+import Route from 'react-router/Route'
+import Link from './Link'
+
+/**
+ * A <Link> wrapper that knows if it's "active" or not.
+ */
+const NavLink = ({
+  to,
+  activeClassName,
+  className,
+  activeStyle,
+  style,
+  isActive: getIsActive,
+  ...rest
+}) => (
+  <Route
+    path={typeof to === 'object' ? to.pathname : to}
+    children={({ history, match }) => {
+      const isActive = !!(getIsActive ? getIsActive(match, history.location) : match)
+
+      return (
+        <Link
+          to={to}
+          className={isActive ? [ activeClassName, className ].join(' ') : className}
+          style={isActive ? { ...style, ...activeStyle } : style}
+          {...rest}
+        />
+      )
+    }}
+  />
+)
+
+NavLink.propTypes = {
+  to: Link.propTypes.to,
+  activeClassName: PropTypes.string,
+  className: PropTypes.string,
+  activeStyle: PropTypes.object,
+  style: PropTypes.object,
+  isActive: PropTypes.func
+}
+
+export default NavLink
